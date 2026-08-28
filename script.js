fbea6a1c57ce2ee91fdb0b2bcbea8900baacf275
geslaidzem-create <geslaidzem@gmail.com>
2026-08-28 16:27:17 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 <script src="script.js"></script>
+
+const aiName = "Vision AI" ;
+const totalImages = 1200;
+const justRedy = 300;
+const imagesOneSession = 150;
+const totalSesiion = 4
+
+let totalReadyimages = imagesOneSession*totalsession;
+let allReadyImages = justReady+totalReadyImages
+let remainingImages = totalImages-allReadyImages
+console.log
